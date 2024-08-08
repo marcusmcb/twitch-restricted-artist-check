88c7c2379355d29791b4782f44fb9aab62aa09aa
marcusmcb <marcusmcb@gmail.com>
2024-08-08 13:41:00 -0700

    added restricted artist list & delay on spotify username lookup

--- spotify.js
+++ spotify.js
@@ -1,5 +1,6 @@
 const axios = require('axios')
 const dotenv = require('dotenv')
+const restrictedArtists = require('./artistList')
 
 dotenv.config()
 
@@ -50,8 +51,11 @@ const getSpotifyUserName = async (accessToken, userId) => {
 		return response.data.display_name
 	} catch (error) {
 		console.error('Error fetching user:', error)
+		return 'user name unavailable'
 	}
 }
+
+const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
 
 ;(async () => {
 	try {
@@ -60,25 +64,51 @@ const getSpotifyUserName = async (accessToken, userId) => {
 		const tracks = await getAllPlaylistTracks(accessToken, playlistId)
 
 		let fullTrackArray = []
+		let restrictedTracks = []
 
-		tracks.forEach(async (track) => {
-			setTimeout(async () => {
-				const addedBy = await getSpotifyUserName(accessToken, track.added_by.id)
+		for (const track of tracks) {
+			const artistNames = track.track.artists.map((artist) => artist.name)
+			const isRestricted = artistNames.some((artist) =>
+				restrictedArtists.includes(artist)
+			)
+
+			if (isRestricted) {
 				const trackEntry = {
 					title: track.track.name,
-					artist: track.track.artists.map((artist) => artist.name).join(', '),
-					added: addedBy !== undefined ? addedBy : 'user name unavailable',
+					artist: artistNames.join(', '),
 				}
-				console.log('Track: ', trackEntry)
-				fullTrackArray.push(trackEntry)
-			}, 1500)
-		})
+				restrictedTracks.push(trackEntry)
+				console.log(
+					'Restricted Track:',
+					track.track.name,
+					'by',
+					artistNames.join(', ')
+				)
+				continue
+			}
 
-		setTimeout(() => {
-			console.log('Full Track Array:')
-			console.log(fullTrackArray)
-		}, 8000)
-    
+			const addedBy = await getSpotifyUserName(accessToken, track.added_by.id)
+			// add spotify URL for each song to the track object
+			const trackEntry = {
+				title: track.track.name,
+				artist: artistNames.join(', '),
+				added: addedBy,
+			}
+			fullTrackArray.push(trackEntry)
+
+			// Delay before the next request
+			await delay(20)
+		}
+
+		console.log('Playable Tracks: ')
+		console.log(fullTrackArray)
+		console.log('------------------------')
+		console.log('Sample Spotify Track Object: ')
+		console.log(tracks[0])
+		console.log('------------------------')
+		console.log('Original Playlist Length: ', tracks.length)
+		console.log('Restricted Track Array: ', restrictedTracks.length)
+		console.log('Clean Track Array: ', fullTrackArray.length)
 	} catch (error) {
 		console.error('Error fetching playlist tracks:', error)
 	}
