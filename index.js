a13ee1abb13c4b21a3405788a055ea7838e04a75
marcusmcb <marcusmcb@gmail.com>
2024-08-08 17:12:04 -0700

    debugging token issue on new playlist creation

--- index.js
+++ index.js
@@ -29,67 +29,67 @@ const getAuthToken = async () => {
 	}
 }
 
-const getArtistStatus = async () => {
-	const url = 'https://gql.twitch.tv/gql'
-  // const params = new URLSearchParams()
-  // params.append('client_id', process.env.CLIENT_ID)
-  // params.append('client_secret', process.env.CLIENT_SECRET)
-  // params.append('authorization:', authToken)
+// const getArtistStatus = async () => {
+// 	const url = 'https://gql.twitch.tv/gql'
+//   // const params = new URLSearchParams()
+//   // params.append('client_id', process.env.CLIENT_ID)
+//   // params.append('client_secret', process.env.CLIENT_SECRET)
+//   // params.append('authorization:', authToken)
 
-	const headers = {
-		'Content-Type': 'text/plain;charset=UTF-8',
-		'Authorization': authToken, // Replace with your OAuth token
-		'Client-ID': process.env.TWITCH_CLIENT_ID, // Replace with your Client ID
-		'User-Agent':
-			'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36',
-		Accept: '*/*',
-		'Accept-Encoding': 'gzip, deflate, br, zstd',
-		'Accept-Language': 'en-US',
-		Origin: 'https://dashboard.twitch.tv',
-		Referer: 'https://dashboard.twitch.tv/',
-		'Sec-Fetch-Site': 'same-site',
-		'Sec-Fetch-Mode': 'cors',
-		'Sec-Fetch-Dest': 'empty',
-		'Sec-Ch-Ua':
-			'"Not)A;Brand";v="99", "Google Chrome";v="127", "Chromium";v="127"',
-		'Sec-Ch-Ua-Mobile': '?0',
-		'Sec-Ch-Ua-Platform': '"Windows"',
-	}
+// 	const headers = {
+// 		'Content-Type': 'text/plain;charset=UTF-8',
+// 		'Authorization': authToken, // Replace with your OAuth token
+// 		'Client-ID': process.env.TWITCH_CLIENT_ID, // Replace with your Client ID
+// 		'User-Agent':
+// 			'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36',
+// 		Accept: '*/*',
+// 		'Accept-Encoding': 'gzip, deflate, br, zstd',
+// 		'Accept-Language': 'en-US',
+// 		Origin: 'https://dashboard.twitch.tv',
+// 		Referer: 'https://dashboard.twitch.tv/',
+// 		'Sec-Fetch-Site': 'same-site',
+// 		'Sec-Fetch-Mode': 'cors',
+// 		'Sec-Fetch-Dest': 'empty',
+// 		'Sec-Ch-Ua':
+// 			'"Not)A;Brand";v="99", "Google Chrome";v="127", "Chromium";v="127"',
+// 		'Sec-Ch-Ua-Mobile': '?0',
+// 		'Sec-Ch-Ua-Platform': '"Windows"',
+// 	}
 
-	const body = JSON.stringify([
-		{
-			operationName: 'DJMusicCatalogSearchQuery',
-			variables: {
-				searchInput: {
-					searchType: 'ARTIST',
-					sortBy: 'BEST_MATCH',
-					term: 'Madonna',
-					channelID: process.env.TWITCH_CHANNEL_ID, 
-				},
-			},
-			extensions: {
-				persistedQuery: {
-					version: 1,
-					sha256Hash:
-						'bf84de47edcd146c548e6fc10664441cf32940bab09af63a23f31bedc32bda2c',
-				},
-			},
-		},
-	])
+// 	const body = JSON.stringify([
+// 		{
+// 			operationName: 'DJMusicCatalogSearchQuery',
+// 			variables: {
+// 				searchInput: {
+// 					searchType: 'ARTIST',
+// 					sortBy: 'BEST_MATCH',
+// 					term: 'Madonna',
+// 					channelID: process.env.TWITCH_CHANNEL_ID, 
+// 				},
+// 			},
+// 			extensions: {
+// 				persistedQuery: {
+// 					version: 1,
+// 					sha256Hash:
+// 						'bf84de47edcd146c548e6fc10664441cf32940bab09af63a23f31bedc32bda2c',
+// 				},
+// 			},
+// 		},
+// 	])
 
-	try {
-    console.log('HEADERS: ', headers)
-    console.log('BODY: ', body)
-		const response = await axios.post(url, body, {
-			headers: headers,
-		})
+// 	try {
+//     console.log('HEADERS: ', headers)
+//     console.log('BODY: ', body)
+// 		const response = await axios.post(url, body, {
+// 			headers: headers,
+// 		})
 
-		console.log(JSON.stringify(response.data, null, 2))
-	} catch (error) {
-    console.log(process.env.TWITCH_CLIENT_ID)
-		console.error('Error:', error.response.data)    
-	}
-}
+// 		console.log(JSON.stringify(response.data, null, 2))
+// 	} catch (error) {
+//     console.log(process.env.TWITCH_CLIENT_ID)
+// 		console.error('Error:', error.response.data)    
+// 	}
+// }
 
 app.get('/', async (req, res) => {
 	res.send('Hello World!')
--- spotify.js
+++ spotify.js
@@ -7,6 +7,7 @@ dotenv.config()
 const clientId = process.env.SPOTIFY_CLIENT_ID
 const clientSecret = process.env.SPOTIFY_CLIENT_SECRET
 const playlistId = process.env.SPOTIFY_PLAYLIST_ID
+const userId = process.env.SPOTIFY_USER_ID
 
 const getAccessToken = async () => {
 	const response = await axios.post(
@@ -55,6 +56,51 @@ const getSpotifyUserName = async (accessToken, userId) => {
 	}
 }
 
+const createNewPlaylist = async (accessToken, userId, playlistName) => {
+	try {
+		const response = await axios.post(
+			`https://api.spotify.com/v1/users/${userId}/playlists`,
+			{
+				name: playlistName,
+				description: 'A playlist free of restricted artists',
+				public: false,
+			},
+			{
+				headers: {
+					Authorization: `Bearer ${accessToken}`,
+					'Content-Type': 'application/json',
+				},
+			}
+		)
+		if (response.data.id) {
+			console.log('New playlist created successfully!')
+		}
+		return response.data.id
+	} catch (error) {
+		console.error('Error creating new playlist:', error)
+	}
+}
+
+const addTracksToPlaylist = async (accessToken, playlistId, trackUris) => {
+	try {
+		await axios.post(
+			`https://api.spotify.com/v1/playlists/${playlistId}/tracks`,
+			{
+				uris: trackUris,
+			},
+			{
+				headers: {
+					Authorization: `Bearer ${accessToken}`,
+					'Content-Type': 'application/json',
+				},
+			}
+		)
+		console.log('Tracks added to the playlist successfully!')
+	} catch (error) {
+		console.error('Error adding tracks to the playlist:', error)
+	}
+}
+
 const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
 
 ;(async () => {
@@ -65,6 +111,7 @@ const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
 
 		let fullTrackArray = []
 		let restrictedTracks = []
+		let trackUris = []
 
 		for (const track of tracks) {
 			const artistNames = track.track.artists.map((artist) => artist.name)
@@ -88,24 +135,41 @@ const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
 			}
 
 			const addedBy = await getSpotifyUserName(accessToken, track.added_by.id)
-			// add spotify URL for each song to the track object
 			const trackEntry = {
 				title: track.track.name,
 				artist: artistNames.join(', '),
 				added: addedBy,
+				spotify_url: track.track.external_urls.spotify,
 			}
 			fullTrackArray.push(trackEntry)
+			trackUris.push(track.track.uri)
 
 			// Delay before the next request
 			await delay(20)
 		}
 
-		console.log('Playable Tracks: ')
-		console.log(fullTrackArray)
-		console.log('------------------------')
-		console.log('Sample Spotify Track Object: ')
-		console.log(tracks[0])
-		console.log('------------------------')
+		// Create a new playlist
+		const newPlaylistId = await createNewPlaylist(
+			accessToken,
+			userId,
+			'Safe Playlist'
+		)
+
+		setTimeout(() => {
+			// Add the safe tracks to the new playlist
+			if (newPlaylistId) {
+				console.log('New Playlist ID: ', newPlaylistId)
+			} else {
+				console.log('No new playlist ID')
+			}
+		}, 1000)
+
+		// Add the safe tracks to the new playlist
+		// await addTracksToPlaylist(accessToken, newPlaylistId, trackUris)
+
+		// console.log('Playable Tracks: ')
+		// console.log(fullTrackArray)
+		// console.log('------------------------')
 		console.log('Original Playlist Length: ', tracks.length)
 		console.log('Restricted Track Array: ', restrictedTracks.length)
 		console.log('Clean Track Array: ', fullTrackArray.length)
